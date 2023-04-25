02ff4df4c4f82806e0a6e40a5174c0bdeb3be846
SJohnRose <shamini.johnrose@gmail.com>
2023-04-25 14:58:20 +1000

    implemented Apollo server in server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+await server.start();
+server.applyMiddleware({ app });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -18,4 +33,9 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+}
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
