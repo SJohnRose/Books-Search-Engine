ad0ca6769a8a061d48fa407575546b686718985a
SJohnRose <shamini.johnrose@gmail.com>
2023-04-29 20:29:05 +1000

    set up saveBook and removeBook mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,11 +1,47 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_USER = gql`
-    mutation createUser($username: String!, $email: String!, $password: String!) {
-        createUser(username: $username, email: $email, password: $password) {
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+            email
+            password
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
             username
             email
             password
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String], $description: String!, $bookId: String!, $image: String, $link: String, $title: String!) {
+        saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+`;
+
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            authors
+            description
+            bookId
+            image
+            link
+            title
         }
     }
 `;
